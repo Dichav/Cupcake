03116db1bbec1080fc0378161eb1e3e0b119a709
Dichav <diana.cha.av@gmail.com>
2017-02-04 02:11:44 -0500

    Add files via upload

---
+++ website/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$(".box p").hide();
+
+	$(".box h4").click(function(e){
+		e.preventDefault();
+		
+		var $this = $(this).parent().find("p");
+		
+		$(".box p").not($this).hide();
+
+		$this.slideToggle("slow");
+	});
+});
+
+
+
